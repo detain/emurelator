df5ab333d469806030e4433990c3b8266e2de103
Joe Huss <detain@interserver.net>
2022-09-08 19:45:46 -0400

    update

--- app/Command/EmulatorCommand.php
+++ app/Command/EmulatorCommand.php
@@ -5,29 +5,23 @@ use CLIFramework\Command;
 
 class EmulatorCommand extends Command {
 	public function brief() {
-		return "Prepairs and works with a source based version of the project (instead of the phar)";
+		return "External sources of data which we will map between each other.";
 	}
 
 	public function execute() {
         echo '
 SYNTAX
 
-emurelator.phar vnc <subcommand>
+emurelator.phar source <subcommand>
 
 SUBCOMMANDS
-	secure [--dry]            removes old and bad entries to maintain security
-	setup <vzid> [ip]         create a new mapping
-	remove <vzid>             remove a mapping
-	restart                   restart the xinetd service
-	rebuild [--dry]           removes old and bad entries to maintain security, and recreates all port mappings
+	list                    displays a list of the current sources
+	add                     adds a new source
+    edit                    update the information on a source
+    del                     remove a source
 
 EXAMPLES
-	emurelator.phar vnc setup vps4000 8.8.8.8
-	emurelator.phar vnc remove vps4000
-	emurelator.phar vnc secure
-	emurelator.phar vnc restart
-	emurelator.phar vnc rebuild --dry
-	emurelator.phar vnc rebuild
+    <none yet>
 ';
 	}
 }
--- app/Command/PlatformCommand.php
+++ app/Command/PlatformCommand.php
@@ -1,15 +1,27 @@
 <?php
 namespace App\Command;
 
-use App\App;
 use CLIFramework\Command;
-use CLIFramework\Formatter;
-use CLIFramework\Logger\ActionLogger;
-use CLIFramework\Debug\LineIndicator;
-use CLIFramework\Debug\ConsoleDebug;
 
 class PlatformCommand extends Command {
 	public function brief() {
-		return "CD Drive/Image functionality";
+		return "External sources of data which we will map between each other.";
+	}
+
+	public function execute() {
+        echo '
+SYNTAX
+
+emurelator.phar source <subcommand>
+
+SUBCOMMANDS
+	list                    displays a list of the current sources
+	add                     adds a new source
+    edit                    update the information on a source
+    del                     remove a source
+
+EXAMPLES
+    <none yet>
+';
 	}
 }
